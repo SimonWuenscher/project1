c280aec4117ab30969ba5e4f3c79cf71192708dd
Simon Wuenscher <luca.wuenscher@gmail.com>
2017-06-14 10:34:58 +0200

    improving the testing method

---
+++ main.cpp
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+int stringcompare(char *s1, char *s2) {
+	int i=0;
+	while(s1[i] == s2[i] && s1[i] && s2[i]) {
+		i++;
+	}
+	return s1[i] - s2[i];
+}
+int main() {
+
+    printf("%d", stringcompare("World", "World"));
+	printf("%d", stringcompare("b", "b"));
+
+
+	return 0;
+}
+
